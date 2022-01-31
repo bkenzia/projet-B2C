0422f6448e3300a333fb76cdf5bee89d678573d3
lylia-toto <bkenzia@yahoo.fr>
2022-01-31 13:32:37 +0100

    ajout 4 realisations sur home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RealisationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(RealisationRepository $realisationRepository): Response
     {
+       $realisations=$realisationRepository->lastRealisation(4);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'realisations' => $realisations,
+            
         ]);
     }
 }
