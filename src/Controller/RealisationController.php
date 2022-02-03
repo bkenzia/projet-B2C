b8477273f3aabbe3c8a013ee5b3fce61e0497990
lylia-toto <bkenzia@yahoo.fr>
2022-02-03 20:13:17 +0100

    ajout de la page realisations + pagination

---
+++ src/Controller/RealisationController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RealisationRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use ContainerPUWjEHs\PaginatorInterface_82dac15;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class RealisationController extends AbstractController
+{
+    #[Route('/realisations', name: 'realisations')]
+    public function realisatios(
+        RealisationRepository $realisationRepository,
+        PaginatorInterface $paginator,
+        Request $request 
+        ): Response {
+            $data=$realisationRepository->findAll();
+            $realisations=$paginator->paginate(
+                $data,
+                $request->query->getInt('page', 1),
+                6
+            );
+
+            $images=array();
+        
+        foreach ($realisations as $realisation) {
+            array_push($images, $realisation->getImages()->toArray()) ;
+             
+        }
+
+        //  dd($images);
+        return $this->render('realisations/index.html.twig', [
+            'realisations' => $realisations,
+            'images'=>$images,
+        ]);
+    }
+}
