71f2382354a48250adbb71c604d0d85a9aab3d83
lylia-toto <bkenzia@yahoo.fr>
2022-02-22 14:09:15 +0100

    gestion des paramètres page apropos

--- src/Controller/AProposController.php
+++ src/Controller/AProposController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class AProposController extends AbstractController
 {
     #[Route('/a-propos', name: 'a_propos')]
-    public function index(): Response
+    public function index(UserRepository $admin): Response
     {
         return $this->render('a_propos/index.html.twig', [
-            'controller_name' => 'AProposController',
+            'admin' => $admin->getAdmin(),
         ]);
     }
 }
